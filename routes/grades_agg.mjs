2ba2170c728387fbc5dba3b85e4ddb00546f98e7
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-11-27 23:39:54 -0500

    first commit

---
+++ routes/grades_agg.mjs
@@ -0,0 +1,87 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+/**
+ * It is not best practice to seperate these routes
+ * like we have done here. This file was created
+ * specifically for educational purposes, to contain
+ * all aggregation routes in one place.
+ */
+
+/**
+ * Grading Weights by Score Type:
+ * - Exams: 50%
+ * - Quizes: 30%
+ * - Homework: 20%
+ */
+//The number of learners with a weighted average (as calculated by the existing routes) higher than 70%
+router.get("/stats", async(req,res)=>{
+  let collection = await db.collection("grades");
+  let result = await collection
+    .aggregate([
+
+      {
+        $unwind: { path: "$scores" },
+      },
+      {
+        $group: {
+          _id: "$class_id",
+          quiz: {
+            $push: {
+              $cond: {
+                if: { $eq: ["$scores.type", "quiz"] },
+                then: "$scores.score",
+                else: "$$REMOVE",
+              },
+            },
+          },
+          exam: {
+            $push: {
+              $cond: {
+                if: { $eq: ["$scores.type", "exam"] },
+                then: "$scores.score",
+                else: "$$REMOVE",
+              },
+            },
+          },
+          homework: {
+            $push: {
+              $cond: {
+                if: { $eq: ["$scores.type", "homework"] },
+                then: "$scores.score",
+                else: "$$REMOVE",
+              },
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          avg: {
+            $sum: [
+              { $multiply: [{ $avg: "$exam" }, 0.5] },
+              { $multiply: [{ $avg: "$quiz" }, 0.3] },
+              { $multiply: [{ $avg: "$homework" }, 0.2] },
+            ],
+          },
+        },
+      },
+      {
+          $match : {avg:{$gte:70}},
+      },
+      {
+        $count: "count"
+      }
+  
+    ])
+    .toArray();
+    console.log(result)
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+    
+});
